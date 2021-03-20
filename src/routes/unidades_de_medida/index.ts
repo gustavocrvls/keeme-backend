8706e8a8f08e6e20bb1c6588d673aaf2b085552e
Gustavo Carvalho Silva <gustavocrvl42@gmail.com>
2021-03-19 23:32:31 -0300

    Remoção do Controller de Unidade de Medida

--- src/routes/unidades_de_medida/index.ts
+++ src/routes/unidades_de_medida/index.ts
@@ -1,12 +1,11 @@
 import { Router } from 'express';
 import PERFIL from '../../constants/Perfil';
-import UnidadeDeMedidaController from '../../controllers/UnidadeDeMedidaController';
 import { verifyToken } from '../../middlewares/auth';
 import { indexUnityOfMeasurementController } from '../../useCases/IndexUnityOfMeasurement';
 
 const routes = Router();
 
-routes.get('/', (req, res) =>
+routes.get('/', verifyToken([PERFIL.ADMINISTRADOR]), (req, res) =>
   indexUnityOfMeasurementController.handle(req, res),
 );
 
